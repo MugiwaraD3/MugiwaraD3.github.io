efb6191b269aaead7083862e6f220daed2c0efff
Thomas Sloboda <thomas.sloboda@xbrainsoft.com>
2014-03-31 12:03:26 +0200

    MugiwaraD3 post system

--- scripts/app/controllers/main.js
+++ scripts/app/controllers/main.js
@@ -1,5 +1,25 @@
-window.angular.module(window.config.id).controller('mainController', ['$scope',
-  function($scope) {
-    $scope.title = 'MD3';
+window.angular.module(window.config.id).controller('mainController', ['$scope', '$firebase',
+  function($scope, $firebase) {
+    var postsRef = new Firebase('https://thomassloboda.firebaseio.com/md3_posts');
+    $scope.postProvider = $firebase(postsRef);
+    $scope.posts = [];
+    
+    postsRef.on('child_added', function(newChild) {
+      
+      $scope.posts.push(newChild.val());
+    });
+
+    $scope.newPostTitle = '';
+    $scope.newPostBody = '';
+    $scope.addPost = function() {
+      $scope.postProvider.$add({
+        id: Guid(),
+        title: $scope.newPostTitle,
+        body: $scope.newPostBody.convertURI(),
+        created: new Date()
+      });
+      $scope.newPostTitle = '';
+      $scope.newPostBody = '';
+    }
   }
 ]);
--- scripts/app/core.js
+++ scripts/app/core.js
@@ -13,6 +13,25 @@ String.prototype.format = function() {
   }
   return ret;
 };
+
+String.prototype.convertURI = function() {
+  var re = /(\(.*?)?\b((?:https?|ftp|file):\/\/[-a-z0-9+&@#\/%?=~_()|!:,.;]*[-a-z0-9+&@#\/%=~_()|])/ig;
+  return this.replace(re, function(match, lParens, url) {
+    var rParens = '';
+    lParens = lParens || '';
+    var lParenCounter = /\(/g;
+    while (lParenCounter.exec(lParens)) {
+      var m;
+      if (m = /(.*)(\.\).*)/.exec(url) ||
+        /(.*)(\).*)/.exec(url)) {
+        url = m[1];
+        rParens = m[2] + rParens;
+      }
+    }
+    return lParens + "<a href='" + url + "' target='__blank'>" + url + "</a>" + rParens;
+  });
+}
+
 window.NodeList.prototype.forEach = Array.prototype.forEach;
 Array.prototype.remove = function() {
   var i;
@@ -23,4 +42,14 @@ Array.prototype.remove = function() {
   }
   return this;
 };
-window.angular.module(window.config.id, []);
+
+function Guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+}
+
+window.angular.module(window.config.id, ['firebase','ngSanitize']);
